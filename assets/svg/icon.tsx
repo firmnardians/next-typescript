2a68adf6fe4e2bbfaf41704f7940c4d0392e5f6e
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-10-04 22:36:17 +0700

    chore: add icons

---
+++ assets/svg/icon.tsx
@@ -0,0 +1,37 @@
+export const IconHome = () => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='#ffffff'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		>
+			<path d='M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z'></path>
+			<polyline points='9 22 9 12 15 12 15 22'></polyline>
+		</svg>
+	);
+};
+
+export const IconLayout = () => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='#ffffff'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		>
+			<rect x='3' y='3' width='18' height='18' rx='2' />
+			<path d='M3 15h18' />
+		</svg>
+	);
+};
