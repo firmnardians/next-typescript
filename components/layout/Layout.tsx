597dc0d02da6bf13e381da6b9fd1d1824870febe
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-10-04 22:51:58 +0700

    feat: add title at layout component

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,5 +1,11 @@
 import React, { ReactNode } from 'react';
+import { LayoutWrapper } from './styled/layout.styled';
 
-export default function Layout({ children }: { children: ReactNode }) {
-	return <main>{children}</main>;
+export default function Layout({ children, title }: { children: ReactNode; title: string }) {
+	return (
+		<LayoutWrapper>
+			<h2>{title}</h2>
+			{children}
+		</LayoutWrapper>
+	);
 }
---
+++ components/layout/styled/layout.styled.tsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const LayoutWrapper = styled.main`
+	max-width: 670px;
+	margin: auto;
+`;
