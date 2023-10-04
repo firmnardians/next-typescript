059415d5d3154ab7095e02b886eb156593185c90
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-10-04 22:35:08 +0700

    chore: project init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Container from '../components/container/Container';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<Container>
+			<Component {...pageProps} />
+		</Container>
+	);
 }
 
-export default MyApp
+export default MyApp;
