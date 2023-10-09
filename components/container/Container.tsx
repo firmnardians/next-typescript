b35506eefffb1f518e4e9b831ecaa74dc5c9ee02
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-10-09 22:43:27 +0700

    feat: use profile page

--- components/container/Container.tsx
+++ components/container/Container.tsx
@@ -1,12 +1,21 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, memo } from 'react';
 import Sidebar from '../sidebar/Sidebar';
 import { ContainerWrapper } from './styled/container.styled';
+import { useRouter } from 'next/router';
+import Profile from '../profile/Profile';
 
-export default function Container({ children }: { children: ReactNode }) {
+const _Container = ({ children }: { children: ReactNode }) => {
+	const router = useRouter();
+	const { pathname } = router;
+
 	return (
 		<ContainerWrapper>
 			<Sidebar />
 			<div className='container'>{children}</div>
+			{pathname === '/' && <Profile />}
 		</ContainerWrapper>
 	);
-}
+};
+
+const Container = memo(_Container);
+export default Container;
