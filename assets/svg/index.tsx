2e2e7d684fea3541f094c1a0fda4bf9cda228df4
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-10-09 22:26:59 +0700

    chore: create SVG file

---
+++ assets/svg/index.tsx
@@ -0,0 +1,11 @@
+export const TagSVG = () => {
+	return (
+		<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' viewBox='0 0 150 150' fill='none'>
+			<path
+				opacity='0.06'
+				d='M0 10C0 4.47715 4.47715 0 10 0H140C145.523 0 150 4.47715 150 10V140C150 145.523 145.523 150 140 150H10C4.47715 150 0 145.523 0 140V10Z'
+				fill='white'
+			/>
+		</svg>
+	);
+};
