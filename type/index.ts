ae59da737cbacc24bce8a8760eb4d840dabd2bae
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-10-09 22:43:58 +0700

    chore: add type file

---
+++ type/index.ts
@@ -0,0 +1,5 @@
+import { ButtonHTMLAttributes, InputHTMLAttributes } from 'react';
+
+export type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export type InputType = InputHTMLAttributes<HTMLInputElement>;
