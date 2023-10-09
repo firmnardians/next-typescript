fae781ad1bf42b733623a38fc73a751f6f458b31
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-10-09 22:51:00 +0700

    chore: update data type

--- type/index.ts
+++ type/index.ts
@@ -3,3 +3,11 @@ import { ButtonHTMLAttributes, InputHTMLAttributes } from 'react';
 export type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export type InputType = InputHTMLAttributes<HTMLInputElement>;
+
+export type ProfileSourceType = Array<{
+	id: number;
+	image: string;
+	fullname: string;
+	username: string;
+	is_following: boolean;
+}>;
